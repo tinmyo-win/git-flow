c2114e8ed435a4b2a9e1011ec76284f6fd7a9f96
Tin Myo Win <tinmyowin@Tins-MacBook-Air.local>
2025-08-28 09:06:44 +0700

    merging to one commit

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const multiply = (a, b) => a * b;
 
 console.log('The result of 2 + 3 is: ' + add(2, 3));
 console.log('The result of 2 * 3 is: ' + multiply(2, 3));
+
+module.exports = {
+  add,
+  multiply
+};
