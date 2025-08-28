7af3a3dfd342ab2d8d0c2e92f91b7bdefc882b80
Tin Myo Win <tinmyowin@Tins-MacBook-Air.local>
2025-08-28 09:31:24 +0700

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const add = (a, b) => a + b;
 const multiply = (a, b) => a * b;
 
 console.log('The result of 2 + 3 is: ' + add(2, 3));
-console.log('The result of 2 * 3 is: ' + multiply(2, 3) );
+console.log('The result of 2 * 3 is: ' + multiply(2, 3));
 
 module.exports = {
   add,
