c6bd0ff496324725acb1e25f6043f18208a5dbd0
Tin Myo Win <tinmyowin@Tins-MacBook-Air.local>
2025-08-29 18:46:39 +0700

    Lave

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const add = (a, b) => a + b;
 
+const addFromArray = (arr) => arr.reduce((acc, curr) => add(acc, curr), 0);
+
 console.log('The result of 2 + 3 is: ' + add(2, 3));
+console.log('The result of [1, 2, 3] is: ' + addFromArray([1, 2, 3]));
